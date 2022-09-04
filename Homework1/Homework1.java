72a6979811d43c88eefca31491a52b4e39029fe2
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-09-04 22:54:54 +0300

    Homework 1 finish

--- Homework1/Homework1.java
+++ Homework1/Homework1.java
@@ -1,5 +1,72 @@
-package Homework1;
+import java.util.Scanner;
+class Homework1 {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        char word;
+        double num1 = Num();
+        double num2 = Num();
+        char operation = Operation();
+        double result = Calculator(num1,num2,operation);
+        System.out.println("Результат операции: " + result);
+        System.out.println("Хотите продолжить?");
+        word = scanner.next().charAt(0);
+        while (word == 'D' || word == 'd'){
+            num1 = Num();
+            num2 = Num();
+            operation = Operation();
+            result = Calculator(num1,num2,operation);
+            System.out.println("Результат операции: " + result);
+            System.out.println("Хотите продолжить?");
+            word = scanner.next().charAt(0);
+        }
+    }
 
-public class Homework1 {
+    public static double Num(){
+        System.out.println("Введите число:");
+        double numFirst;
+        if(scanner.hasNextDouble()){
+            numFirst = scanner.nextDouble();
+        } else {
+            System.out.println("Допущена ошибка.Введите еще раз");
+            scanner.next();
+            numFirst = Num();
+        }
+        return numFirst;
+    }
+
+    public static char Operation(){
+        System.out.println("Введите желаемую операцию: ");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Допущена ошибка.Введите еще раз");
+            scanner.next();//рекурсия
+            operation = Operation();
+            }
+            return operation;
+    }
     
+    public static double Calculator(double numberOne, double numTwo, char operation){
+        double result;
+        switch (operation){
+            case '+':
+                result = numberOne + numTwo;
+                break;
+            case '-':
+                result = numberOne - numTwo;
+                break;
+            case '*':
+                result = numberOne * numTwo;
+                break;
+            case '/':
+                result = numberOne / numTwo;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = Calculator(numberOne, numTwo, Operation()); //рекурсия
+        }
+        return result;
+    }
+
 }
