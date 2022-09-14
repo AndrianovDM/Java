ea89417989515bbb505115cf22e466bafdaa8ed4
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-09-15 02:44:11 +0300

    add task 1

---
+++ Seminar2/Seminar2.java
@@ -0,0 +1,21 @@
+// // import java.util.Scanner;
+// import java.util.logging.Logger;
+
+// public class Seminar2{
+//     public static void main(String[] args) {
+
+//         Logger l = Logger.getAnonymousLogger();
+//         String str = "ab";
+//         l.info(concat(2, str));
+        
+//     }
+//     private static String concat(int n, String string){
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < n; i++){
+//             sb.append(string);
+//         }
+//         return sb.toString();
+
+//     } 
+// }
+    
